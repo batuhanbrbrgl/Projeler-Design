bc502d48ff25bbfd40c022ecace40c5d4d0fb86f
Batuhan BERBEROĞLU <bthnnapcan@gmail.com>
2022-09-05 17:23:47 +0300

    Starbucks Site Tasarım

---
+++ starbucks/ui.js
@@ -0,0 +1,8 @@
+function imgSlider(anything) {
+    document.querySelector(".starbucks").src = anything;
+}
+
+function changecolor(color) {
+    let circle = document.querySelector(".circle")
+    circle.style.backgroundColor = color;
+}
